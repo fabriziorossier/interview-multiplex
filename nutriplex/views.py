08e080d88cc2bc7b70681b8b1c776ac154da5122
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-11-21 18:47:28 -0300

    fix: updated for OpenAI v1.0.0 SDK

--- nutriplex/views.py
+++ nutriplex/views.py
@@ -1,5 +1,5 @@
 import os
-import openai
+from openai import OpenAI
 from django.shortcuts import render
 from django.shortcuts import redirect
 from rest_framework import generics
@@ -40,14 +40,24 @@ def send_to_openai(request):
             selected_in_category = request.POST.getlist(category.name)
             selected_items.extend(selected_in_category)
         
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        client = OpenAI(
+            api_key=os.environ['OPENAI_API_KEY']
+        )
 
-        message = f"Necesito una receta con los siguientes ingredientes: {', '.join(selected_items)}, ademas necesito las cantidades de cada ingrediente para la receta, los pasos para realizar la receta y el aporte calorico total de la receta, formatea la receta con codigo HTML utilizando solo tags h2, h3, ul, ol, li, y p, centrando en la pantalla los elementos h2"
+        message = f"Necesito una receta con los siguientes ingredientes: {', '.join(selected_items)}, ademas necesito las cantidades de cada ingrediente para la receta, los pasos para realizar la receta y el aporte calorico total de la receta, formatea la receta con codigo HTML utilizando solo tags h2, h3, ul, ol, li, y p, centrando en la pantalla los elementos h2. No indiques en la pagina informacion sobre HTML o los tags HTML."
 
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
+        response = client.chat.completions.create(
+            model="gpt-4o",
             messages=[
-                {"role": "user", "content": message}
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                        "type": "text",
+                        "text": message
+                        }
+                    ]
+                }
             ]
         )
 
