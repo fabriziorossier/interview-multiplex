4d1ce2261b2ab8e085e6f603e6a818ffc8abac94
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-11-25 19:20:43 -0300

    modified base url

--- nutriplex/urls.py
+++ nutriplex/urls.py
@@ -4,7 +4,7 @@ from .views import select_items
 from . import views
 
 urlpatterns = [
-    path('nutriplex/', select_items, name='select-items'),
+    path('', select_items, name='select-items'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('recipe/', views.send_to_openai, name='send_to_openai'),
     re_path(r'^.*$', views.page_not_found, name='page_not_found'),
